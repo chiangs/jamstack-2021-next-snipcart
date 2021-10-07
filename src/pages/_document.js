144d7b8ed247c0e96b724e88c0c4391875dc9a85
Stephen Chiang <stephen.chiang@webstep.no>
2021-10-07 21:14:55 +0200

    customize cart, checkout, form components and styles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,6 +29,7 @@ class MyDocument extends Document {
                         id='snipcart'
                         data-api-key={process.env.NEXT_PUBLIC_SNIPCART_API_KEY}
                         data-config-modal-style='side'
+                        data-templates-url='/snipcart-templates.html'
                     ></div>
                 </body>
             </Html>
